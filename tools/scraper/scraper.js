d086a1740bab5b0cb69e6041037276100185e63a
Will Blanchette <will@unata.com>
2014-01-11 11:20:06 -0500

    Revamped caching system and tidied up some regular expressions

--- tools/scraper/scraper.js
+++ tools/scraper/scraper.js
@@ -5,31 +5,34 @@ var cheerio = require("cheerio");
 var hash = require("MD5");
 var http = require("http-get");
 
-var fetch = function(url, callback){
-    fs.exists(".cache", function(exists){
-        if(!exists){
-            fs.mkdirSync(".cache");
-        }
+fs.exists(".cache", function(exists){
+    if(!exists){
+        fs.mkdirSync(".cache");
+        fs.mkdirSync(".cache/html");
+        fs.mkdirSync(".cache/images");
+        fs.mkdirSync(".cache/json");
+    }
+});
 
-        fs.exists(".cache/" + hash(url), function(exists){
-            if(exists){
-                console.log("loading from cache");
-                fs.readFile(".cache/" + hash(url), "utf-8", function(error, cache){
-                    callback(cache);
+var fetch = function(url, callback){
+    fs.exists(".cache/html/" + hash(url), function(exists){
+        if(exists){
+            console.log("loading from cache");
+            fs.readFile(".cache/html/" + hash(url), "utf-8", function(error, cache){
+                callback(cache);
+            });
+        }else{
+            console.log("loading from web")
+            request(url, function(error, response, body){
+                fs.writeFile(".cache/html/" + hash(url), body, function(){
+                    callback(body);
                 });
-            }else{
-                console.log("loading from web")
-                request(url, function(error, response, body){
-                    fs.writeFile(".cache/" + hash(url), body, function(){
-                        callback(body);
-                    });
-                });
-            }
-        });
+            });
+        }
     });
 };
 
-var baseHREF = "http://www.previewsworld.com/"
+var baseHREF = "http://www.previewsworld.com/";
 
 fetch(baseHREF, function(html){
     var $ = cheerio.load(html);
@@ -57,24 +60,21 @@ fetch(baseHREF, function(html){
                 publisher: $(".StockCodePublisher").text().replace(/Publisher:\W+/, ""),
                 itemCode: $(".StockCodeDiamdNo").text().replace(/Item Code:\W+/, ""),
                 sellDate: $(".StockCodeInShopsDate").text().replace(/In Shops:\W+/, ""),
-                price: $(".StockCodeSrp").text().replace(/SRP:\W+/, ""),
+                price: $(".StockCodeSrp").text().replace(/(SRP:\W+)|\s+/g, ""),
                 description: $(".PreviewsHtml").text(),
                 creators: $(".StockCodeCreators").text()
             };
 
+            console.log("Saving json");
+            fs.writeFileSync(".cache/json/" + product.itemCode + ".json", JSON.stringify(product));
+
             var imageHREF = baseHREF + $(".FancyPopupImage").attr("href");
 
-            fs.exists("images", function(exists){
-                if(!exists){
-                    fs.mkdirSync("images");
-                }
-            });
-
-            fs.exists("images/" + product.itemCode + ".jpg", function(exists){
+            fs.exists(".cache/images/" + product.itemCode + ".jpg", function(exists){
                 if(!exists){
                     console.log("fetching image");
-                    http.get(imageHREF, "images/" + product.itemCode + ".jpg", function(error, result){
-
+                    http.get(imageHREF, ".cache/images/" + product.itemCode + ".jpg", function(error, result){
+                        console.log("fetched image and saved in cache");
                     });
                 }
             });
